5449d3764026bf34dd3ac3323da6fc2862c1ea57
Harsha0110 <92736755+Harsha0110@users.noreply.github.com>
2024-12-15 21:37:02 +0530

    Update Create.js

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
 import {useNavigate} from "react-router-dom"
-import Create.css
+import './create.css';
+
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Create = () => {
